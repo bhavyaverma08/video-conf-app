81d295c56f8d51d7b67dfa03af5cdea2ad821ae4
bhavyaverma08 <verma100bhavya@gmail.com>
2024-05-16 12:20:15 +0530

    Initial changes with firestore addition

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuRpnpi3hNKpRBS1PJcVwBc6Cqh2PeWmg",
+  authDomain: "video-conference-app-f64c3.firebaseapp.com",
+  projectId: "video-conference-app-f64c3",
+  storageBucket: "video-conference-app-f64c3.appspot.com",
+  messagingSenderId: "220625287971",
+  appId: "1:220625287971:web:4c6f3c8bfc0ef9d82cea96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
